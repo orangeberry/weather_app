0dd7bfa88312e5ffa69379154d79f6fd54a242e0
orangeberry <orangeberry@kakao.com>
2021-01-11 01:53:16 +0900

    uploads

---
+++ Weather.js
@@ -0,0 +1,41 @@
+import React from "react";
+import {View, Text, StyleSheet} from "react-native";
+import PropTypes from "prop-types";
+
+export default function Weather({temp}){
+    return (
+    <View style={styles.container}>
+        <Text>{temp}</Text>
+    </View>
+    )
+}
+
+Weather.PropTypes = {
+    tmep:PropTypes.number.isRequired,
+    conditon: PropTypes.oneOf([
+        "Thunderstorm",
+        "Drizzle",
+        "Rain",
+        "Snow",
+        "Atmosphere",
+        "Clear",
+        "Clouds",
+        "Haze",
+        "Mist",
+        "Smoke",
+        "Dust",
+        "Fog",
+        "Sand",
+        "Ash",
+        "Squall",
+        "Tornado"
+    ]).isRequired
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    }
+})
